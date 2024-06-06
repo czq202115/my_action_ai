29c302b3c3ab26328015039309c93125b0220173
czq202115 <136564126+czq202115@users.noreply.github.com>
2024-06-07 02:12:08 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@
 
 # 启动 Docker 容器
 sudo docker run  -itd --name chat -v $GITHUB_WORKSPACE/models/:/models/ -p 8000:8000  my-image:latest -m /models/MiniCPM-2B-dpo-fp16-gguf.gguf --port 8000 --host 0.0.0.0 -n 1024
-sudo docker run -itd --name cf cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiYWZjODQ0MzQ0YzNhZGE5ODM3MzgzYjFlNDQzYjg3YWEiLCJ0IjoiMTQyZDMzMWMtYWUzZC00ZGJkLThhYzktZDJlNzY2YTAxYmFiIiwicyI6Ill6YzRNRGhpWVRBdE0yRmpOQzAwWW1RNUxUbGhOVFF0WXpSbE16WmlOMlF5TXpOaiJ9
+sudo docker run -itd --network host --name cf cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiYWZjODQ0MzQ0YzNhZGE5ODM3MzgzYjFlNDQzYjg3YWEiLCJ0IjoiMTQyZDMzMWMtYWUzZC00ZGJkLThhYzktZDJlNzY2YTAxYmFiIiwicyI6Ill6YzRNRGhpWVRBdE0yRmpOQzAwWW1RNUxUbGhOVFF0WXpSbE16WmlOMlF5TXpOaiJ9
 # 获取当前时间戳
 start_time=$(date +%s)
 
