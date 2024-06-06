337161b2fa1102290781ea5a7e6a180f477f364d
czq202115 <136564126+czq202115@users.noreply.github.com>
2024-06-07 01:48:38 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 启动 Docker 容器
-docker run  -itd --name chat -v $GITHUB_WORKSPACE/:/models -p 8000:8000 ghcr.io/ggerganov/llama.cpp:server -m /models/MiniCPM-2B-dpo-fp16-gguf --port 8000 --host 0.0.0.0 -n 1024
-
+sudo docker run  -itd --name chat -v $GITHUB_WORKSPACE/:/models -p 8000:8000 ghcr.io/ggerganov/llama.cpp:server -m /models/MiniCPM-2B-dpo-fp16-gguf.gguf --port 8000 --host 0.0.0.0 -n 1024
+sudo docker run -itd --name cf cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiYWZjODQ0MzQ0YzNhZGE5ODM3MzgzYjFlNDQzYjg3YWEiLCJ0IjoiMTQyZDMzMWMtYWUzZC00ZGJkLThhYzktZDJlNzY2YTAxYmFiIiwicyI6Ill6YzRNRGhpWVRBdE0yRmpOQzAwWW1RNUxUbGhOVFF0WXpSbE16WmlOMlF5TXpOaiJ9
 # 获取当前时间戳
 start_time=$(date +%s)
 
 # 计算 4 小时 55 分钟后的时间戳
-end_time=$((start_time + 4*60*60 + 55*60))
+end_time=$((start_time + 2*60*60 + 55*60))
 
 # 循环检查时间，直到当前时间超过结束时间
 while [ $(date +%s) -lt $end_time ]; do
@@ -16,7 +16,9 @@ while [ $(date +%s) -lt $end_time ]; do
 done
 
 # 当时间超过 4 小时 55 分钟后，停止 Docker 容器
-docker stop chat
+sudo docker stop chat
+sudo docker stop cf
+sudo docker rm chat cf
 
 
 
