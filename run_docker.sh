68e13a5e8941f1c7164d4f0a30e6fd7135624bd9
czq202115 <136564126+czq202115@users.noreply.github.com>
2024-06-07 10:47:14 +0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# 创建自定义网络（如果尚未创建）
+network_name="my_custom_network"
+subnet="172.18.0.0/16"
+
+docker network inspect $network_name > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    docker network create --subnet=$subnet $network_name
+fi
+
+# 指定容器的静态IP地址
+static_ip1="172.18.0.10"
+static_ip2="172.18.0.11"
+
 # 启动 Docker 容器
-sudo docker run  -itd --name chat -v $GITHUB_WORKSPACE/models/:/models/ -p 8000:8000  my-image:latest -m /models/MiniCPM-2B-dpo-fp16-gguf.gguf --port 8000 --host 0.0.0.0 -n 1024
+docker run -itd --name cf --net $network_name --ip $static_ip1 cloudflare/cloudflared:latest tunnel --no-autoupdate run --token eyJhIjoiYWZjODQ0MzQ0YzNhZGE5ODM3MzgzYjFlNDQzYjg3YWEiLCJ0IjoiMTQyZDMzMWMtYWUzZC00ZGJkLThhYzktZDJlNzY2YTAxYmFiIiwicyI6Ill6YzRNRGhpWVRBdE0yRmpOQzAwWW1RNUxUbGhOVFF0WXpSbE16WmlOMlF5TXpOaiJ9
+sudo docker run  -itd --name chat --net $network_name --ip $static_ip2 -v $GITHUB_WORKSPACE/models/:/models/ -p 8000:8000  my-image:latest -m /models/MiniCPM-2B-dpo-fp16-gguf.gguf --port 8000 --host 0.0.0.0 -n 1024
 # 获取当前时间戳
 start_time=$(date +%s)
 
 # 计算 4 小时 55 分钟后的时间戳
-end_time=$((start_time + 2*60*60 + 55*60))
+end_time=$((start_time + 5*60*60 + 55*60))
 
 # 循环检查时间，直到当前时间超过结束时间
 while [ $(date +%s) -lt $end_time ]; do
@@ -16,7 +30,8 @@ done
 
 # 当时间超过 4 小时 55 分钟后，停止 Docker 容器
 sudo docker stop chat
-sudo docker rm chat 
+sudo docker stop cf 
+sudo docker rm chat cf
 
 
 
